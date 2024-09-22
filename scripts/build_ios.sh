d5c9d6d6bebc1a6f7e521d9449e760349c480531
Robby Sarvis <rjs.app.dev@proton.me>
2024-09-22 11:31:45 -0500

    add args to the build_ios.sh script

--- scripts/build_ios.sh
+++ scripts/build_ios.sh
@@ -6,7 +6,7 @@ rm -rf ChemistryEquationsBackbone
 rm -rf ../BackboneSPM/Sources
 rm -rf ../BackboneSPM/RustFramework.xcframework
 
-cargo swift package
+cargo swift package --platforms macos ios --name ChemistryEquationsBackbone
 
 
 cp -R ChemistryEquationsBackbone/Sources ../BackboneSPM/Sources
